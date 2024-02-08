b02e62bb4a9118ab516a87710bdedc5a8f17654c
putriimnida <156336254+putriimnida@users.noreply.github.com>
2024-02-08 11:58:15 +0700

    Add files via upload
    intro to Azure

---
+++ azure-learning.py
@@ -0,0 +1,7 @@
+import os
+import uuid
+import sys
+from azure.storage.blob import BlockBlobService, PublicAccess
+
+blob_service_client = BlockBlobService(account_name='datasetplatinumgenomes', sas_token='sv=2019-02-02&se=2050-01-01T08%3A00%3A00Z&si=prod&sr=c&sig=FFfZ0QaDcnEPQmWsshtpoYOjbzd4jtwIWeK%2Fc4i9MqM%3D')
+blob_service_client.get_blob_to_path('dataset/2017-1.0/hg38/small_variants/NA12877', 'NA12877.vcf.gz', './NA12877.vcf.gz')
